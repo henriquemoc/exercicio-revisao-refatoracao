a20edfc54a9587d450d9018593fcbcc2a5a93a4b
henriquemoc <henriquemoc2@gmail.com>
2020-10-18 23:00:02 -0300

    Update Empregado.cpp

--- Empregado.cpp
+++ Empregado.cpp
@@ -5,7 +5,6 @@ double Empregado::pagamentoMes(double horasTrabalhadas) {
     //Cálculo de hora extra (+50% se horasTrabalhadas > 8)
     if (horasTrabalhadas > 8) {
         double horasExtras = horasTrabalhadas - 8;
-        t += horasExtras / 2;
         return (horasTrabalhadas + (horasExtras/2)) * salarioHora;
     } else {
         return horasTrabalhadas * salarioHora;
